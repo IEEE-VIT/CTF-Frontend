eeba864d5075016fa598b1fb1386ec3a615cb578
vi_mi <fenil.jain2018@vitstudent.ac.in>
2020-02-18 19:43:51 +0530

    Updates

---
+++ precache-manifest.fde854bb88227febce5671033b097ec8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca62966eadeb5bc49fe9a64914a5aa5d",
+    "url": "/ctf/index.html"
+  },
+  {
+    "revision": "19e9534dc0a310970c3b",
+    "url": "/ctf/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9d1201d1f6b694f73d5c",
+    "url": "/ctf/static/js/2.e2c7e54c.chunk.js"
+  },
+  {
+    "revision": "e45fc4e24d8732db7e2536c643149c35",
+    "url": "/ctf/static/js/2.e2c7e54c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19e9534dc0a310970c3b",
+    "url": "/ctf/static/js/main.0fea21e6.chunk.js"
+  },
+  {
+    "revision": "540ebe003f9567fd536f",
+    "url": "/ctf/static/js/runtime-main.6bece4ab.js"
+  },
+  {
+    "revision": "daae3eb200b26af2f98da42818c4e5a3",
+    "url": "/ctf/static/media/globe_dark.daae3eb2.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ctfGlobe/precache-manifest.9257e06e8c63769c607be2d4d4f470a8.js"
+  "/ctf/precache-manifest.fde854bb88227febce5671033b097ec8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ctfGlobe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ctf/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
