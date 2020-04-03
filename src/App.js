3277998760ead9f416ede5311606ee435d307074
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-04-03 14:54:53 +0530

    Landing Page complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Globe from './uiComponents/globe/globe.js';
 
+// importing Screens
+import LandingScreen from './screens/LandingScreen';
+
 import './App.css';
+
 
 class App extends React.Component{
 
   render(){
     return(
-      <div> 
-        <Globe />
-    </div>
+      <LandingScreen />
     );
   }
 }
---
+++ src/screens/LandingScreen/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+// importing StyleSheets
+import './styles.css';
+import '../../Styles.css';
+
+class LandingScreen extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="mainContainer">
+                <img className="ctfLogo" src={require('../../assets/ctfLogo.png')} alt="CTF Logo" height={260} />
+                <button className='platBtn textLight' onClick={() => console.log("Pressed")} >Play</button>
+            </div>
+        );
+    }
+}
+
+
+export default LandingScreen;
