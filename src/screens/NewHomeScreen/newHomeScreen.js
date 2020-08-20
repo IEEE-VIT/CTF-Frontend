999d3e562ca660ee6f3ee24cb7d4a6d93b355fd7
vi_mi <fenil.jain2018@vitstudent.ac.in>
2020-08-20 22:25:07 +0530

    Correcting new home and landing page css

--- src/screens/NewHomeScreen/newHomeScreen.js
+++ src/screens/NewHomeScreen/newHomeScreen.js
@@ -26,38 +26,15 @@ class NewHomeScreen extends Component {
 			return (
 				<React.Fragment>
 					<div className="newLandingScreen__main-container">
-						<nav className="nav">
-							<div className="nav__ctf"><img src={ctfLogo} alt=""/></div>
-							<div className="nav__buttons-container">
-								<div className="nav__button" style={{opacity: page === "map" ? 1 : 0.64}} onClick={()=>{
-									this.setState({
-										page: 'map',
-									});
-								}}>Map</div>
-								<div className="nav__button" style={{opacity: page === "leaderboard" ? 1 : 0.64}} onClick={()=>{
-									this.setState({
-										page: 'leaderboard'
-									});
-								}}>Leaderboard</div>
-								<div className="nav__button" style={{opacity: page === "info" ? 1 : 0.64}} onClick={()=>{
-									this.setState({
-										page: 'info',
-									});
-								}}>Info</div>
-								<div className="nav__button" style={{opacity: page === "profile" ? 1 : 0.64}} onClick={()=>{
-									this.setState({
-										page: 'profile',
-									});
-								}}>Profile</div>
-							</div>
-							<div className="nav__score">Your score: 0</div>
+						<nav className="newHomePage__nav">
+							<div className="newHomePage__nav__ctf"><img src={ctfLogo} alt=""/></div>
 						</nav>
 						<SocialMediaIcons />
-						<LandingGlobe />
 					</div>
 					<div className = 'landing__logo-play'>
 						<div className = 'newHomeScreen__center_text'>We will be live soon!</div>
-						<div className = 'newHomeScreen__center_text'>Stay tuned!</div>
+						<div className = 'newHomeScreen__center_text' style={{"paddingTop": "60px"}}>Stay tuned!</div>
+						<LandingGlobe />
 					</div>
 				</React.Fragment>
 			);
--- src/screens/NewLandingScreen/newLandingScreen.js
+++ src/screens/NewLandingScreen/newLandingScreen.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 //import 'firebase';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 // importing StyleSheets
 import './newLandingScreen.css';
@@ -17,7 +19,7 @@ class NewLandingScreen extends Component {
 	constructor(props) {
 		super(props);
 		this.state={
-			page: 'map',
+			page: 'home',
 		};
 	}
 
@@ -25,43 +27,100 @@ class NewLandingScreen extends Component {
 		const {page} = this.state;
 			return (
 				<React.Fragment>
+					<div>
 					<div className="newLandingScreen__main-container">
-						<nav className="nav">
+						<Navbar collapseOnSelect expand="lg" variant="dark" style={{"fontSize": "21px"}}>
 							<div className="nav__ctf"><img src={ctfLogo} alt=""/></div>
-							<div className="nav__buttons-container">
-								<div className="nav__button" style={{opacity: page === "map" ? 1 : 0.64}} onClick={()=>{
-									this.setState({
-										page: 'map',
-									});
-								}}>Info</div>
-								<div className="nav__button" style={{opacity: page === "leaderboard" ? 1 : 0.64}} onClick={()=>{
-									this.setState({
-										page: 'leaderboard'
-									});
-								}}>History</div>
-								<div className="nav__button" style={{opacity: page === "info" ? 1 : 0.64}} onClick={()=>{
-									this.setState({
-										page: 'info',
-									});
-								}}>Contact</div>
-								<div className="nav__button" style={{opacity: page === "profile" ? 1 : 0.64}} onClick={()=>{
-									this.setState({
-										page: 'profile',
-									});
-								}}>Profile</div>
-							</div>
-							<div className="newLandingScreen__loginBtn" onClick={() => window.location.href = '/get_started'}>Play</div>
-						</nav>
+							<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+							<Navbar.Collapse id="responsive-navbar-nav">
+								<Nav className="mr-auto">
+								</Nav>
+								<Nav>
+									<Nav.Link 
+										onClick={() => {
+										this.setState({
+											page: 'home'
+										});
+									}}
+									>Home</Nav.Link>
+									<Nav.Link 
+										onClick={() => {
+										this.setState({
+											page: 'map'
+										});
+									}}
+									>Info</Nav.Link>
+									<Nav.Link
+										onClick={() => {
+										this.setState({
+											page: 'history'
+										});
+									}}
+									>History</Nav.Link>
+									<Nav.Link
+										onClick={() => {
+										this.setState({
+											page: 'contact'
+										});
+									}}
+									>Contact</Nav.Link>
+									<Nav.Link
+										onClick={() => window.location.href = '/get_started'}
+									>Play</Nav.Link>
+								</Nav>
+							</Navbar.Collapse>
+						</Navbar>
+						{
+							/*
+								<nav className="nav">
+									<div className="nav__ctf"><img src={ctfLogo} alt=""/></div>
+									<div className="nav__buttons-container">
+										<div className="nav__button" style={{opacity: page === "map" ? 1 : 0.64}} onClick={()=>{
+											this.setState({
+												page: 'map',
+											});
+										}}>Info</div>
+										<div className="nav__button" style={{opacity: page === "leaderboard" ? 1 : 0.64}} onClick={()=>{
+											this.setState({
+												page: 'leaderboard'
+											});
+										}}>History</div>
+										<div className="nav__button" style={{opacity: page === "info" ? 1 : 0.64}} onClick={()=>{
+											this.setState({
+												page: 'info',
+											});
+										}}>Contact</div>
+										<div className="nav__button" style={{opacity: page === "profile" ? 1 : 0.64}} onClick={()=>{
+											this.setState({
+												page: 'profile',
+											});
+										}}>Profile</div>
+									</div>
+									<div className="newLandingScreen__loginBtn" onClick={() => window.location.href = '/get_started'}>Play</div>
+								</nav>
+							*/
+						}
 						<SocialMediaIcons />
-						<LandingGlobe />
 					</div>
 					<div className = 'landing__logo-play'>
-						<img className="ctfLogo" src={require('../../assets/ctfLogo.png')} alt="CTF Logo" height={260} />
+							<div className = 'ctfLogo'>
+								{
+									this.state.page === 'home'
+										?
+											<img src={require('../../assets/ctfLogo.png')} alt="CTF Logo" />
+										:
+										<p style={{"color": "white"}}>Some other page</p>
+								}
+
+							</div>
+							<LandingGlobe />
 						{
 							/*
 								<div className="button loginBtn" onClick={() => window.location.href = '/get_started'}>Play</div>
+								<p style={{"fontSize": "500px", "color": "white", "zIndex": "100000", "width": "100vw", "height": "100vh"}}>CTF CTF CTF</p>
 							*/
 						}
+						</div>
 					</div>
 				</React.Fragment>
 			);
